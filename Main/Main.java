2c8b3db43a8e54ca878fdfb3a3b073bca2a50ce8
ThomLam <lamiablethomas@gmail.com>
2021-09-24 13:50:18 +0200

    Rajout des fonctions entre elle. Manque le score et l'aléatorie

---
+++ Main/Main.java
@@ -0,0 +1,15 @@
+package Main;
+
+import Menu.Menu;
+
+import java.util.Scanner;
+
+public class Main {
+    static String[][] table = new String[11][12];
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[]args){
+        Menu.main();
+
+    }
+}
